c098780523b82a1121ea03845d4f1db7ae545d54
Gary Li <garyli0802@yahoo.com>
2022-10-12 12:40:22 -0700

    initial commit

--- APIsAndJSON/Program.cs
+++ APIsAndJSON/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace APIsAndJSON
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using APIsAndJSON;
+
+
+
+namespace APIsAndJSON
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //var client = new HttpClient();
+            //var quotes = new QuoteGenerator(client);
+
+            //Console.WriteLine("Conversation between Kanye and Ron Swanson!!!!");
+
+            //for (int i = 0; i < 5; i++)
+            //{
+
+            //    Console.WriteLine("----------------------------");
+            //    Console.WriteLine($"Kanye: {quotes.Kanye()}");
+            //    Console.WriteLine();
+            //    Console.WriteLine($"Ron Swanson: {quotes.RonSwanson()}");
+            //}
+
+
+            string key = File.ReadAllText("appsettings.json");
+            string APIKey = JObject.Parse(key).GetValue("APIKey").ToString();
+
+            Console.WriteLine("Please enter your city.");
+            var city = Console.ReadLine();
+
+            var apiCall = $"https://api.openweathermap.org/data/2.5/weather?q={city}&units=imperial&appid={APIKey}";
+            Console.WriteLine();
+
+            Console.WriteLine($"It is currently {OpenWeatherMapAPI.GetTemp(apiCall)} degrees F in your location.");
         }
     }
 }
--- APIsAndJSON/RonVSKanyeAPI.cs
+++ APIsAndJSON/RonVSKanyeAPI.cs
@@ -3,10 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
+using static System.Net.WebRequestMethods;
+using Newtonsoft.Json.Linq;
 
 namespace APIsAndJSON
 {
-    internal class RonVSKanyeAPI
+    public class QuoteGenerator
     {
+        private HttpClient _client;
+
+        public QuoteGenerator(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public string Kanye()
+        {
+            // api url
+            var kanyeURL = "https://api.kanye.rest";
+            // stores json response
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+            // parses response with the name quote
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+        public string RonSwanson()
+        {
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+            var ronResponse = _client.GetStringAsync(ronURL).Result;
+            var ronQuote = JArray.Parse(ronResponse);
+
+            return ronQuote[0].ToString();
+
+        }
     }
 }
